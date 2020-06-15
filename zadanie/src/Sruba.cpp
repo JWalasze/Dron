46c0aeb374afec09d6261becea7134c484b85c8e
Jakub Walaszek <252897@student.pwr.edu.pl>
2020-06-15 13:05:45 +0200

    dodany zalazek

--- zadanie/src/Sruba.cpp
+++ zadanie/src/Sruba.cpp
@@ -4,7 +4,7 @@ void Sruba::obroc_z_dronem(MacierzOb Nowa_orientacja_drona)
 {
     MacOrientacji = Nowa_orientacja_drona * Orientacja_mocowania;
     MacOrientacji = MacOrientacji * MacierzOb(Kat_obrotu_sruby, 'x');
-    Kat_obrotu_sruby += 1;
+    //Kat_obrotu_sruby += 1;
 }
 
 void Sruba::przesun_z_dronem(Wektor<double,3> Nowy_sr_drona, MacierzOb Nowa_orientacja_drona)
@@ -15,6 +15,6 @@ void Sruba::przesun_z_dronem(Wektor<double,3> Nowy_sr_drona, MacierzOb Nowa_orie
  Sruba::Sruba(double WspX, double WspY, double WspZ, double WymX, double WymZ) : Graniastoslup(WspX, WspY, WspZ, WymX, WymZ) //Orientacja_mocowania(90,'y')
  {
      Orientacja_mocowania = MacierzOb(0, 'z');
-     Kat_obrotu_sruby = 1;
+     Kat_obrotu_sruby = 30;
      Przesuniecie_mocowania = Wektor<double,3>(WspX,WspY,WspZ);
  }
